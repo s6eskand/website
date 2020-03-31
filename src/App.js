328707857525130a23acbb68abef301b4fd26d7d
Sam <sam.eskandar00@hotmail.com>
2020-03-31 16:06:19 -0400

    add expertise section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from "./components/About";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from "./components/Navbar";
 import Projects from './components/Projects';
+import Expertise from "./components/Expertise";
 
 class App extends React.Component {
   render() {
@@ -14,6 +15,7 @@ class App extends React.Component {
             <Header/>
             <About/>
             <Projects/>
+            <Expertise/>
         </div>
     );
   }
