037a68a9322c6e2632b9215c63bdbb0f9fada935
Sam <sam.eskandar00@hotmail.com>
2020-04-28 17:18:41 -0400

    change nav style

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import Header from "./components/Header";
 import About from "./components/About";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from "./components/Navbar";
+import Navbar2 from "./components/Navbar2";
 import Projects from './components/Projects';
 import Expertise from "./components/Expertise";
 import Contact from "./components/Contact";
 
 class App extends React.Component {
+
   render() {
     return (
         <div>
-            <Navbar/>
+            <Navbar2/>
             <Header/>
             <About/>
             <Projects/>
---
+++ src/components/Navbar2.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import { Link, animateScroll as scroll} from 'react-scroll';
+import ReactCSSTransitionGroup from 'react-transition-group';
+import {FaHome} from 'react-icons/fa';
+
+const navStyle = {
+    // transition: 'navLinkFade 0.5s ease forwards'
+};
+
+class Navbar2 extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            transform: 'translateX(100%)',
+            isViewed: false,
+            opacity: '0'
+        };
+    }
+
+    logoClick = () => {
+        scroll.scrollToTop();
+        if (this.state.isViewed) {
+            this.setState({
+                transform: 'translateX(100%)',
+                opacity:'0'
+            })
+        }
+    };
+
+    navSlide = () => {
+        this.setState({
+            isViewed: !this.state.isViewed,
+            transform: this.state.isViewed ? 'translateX(100%)' : "translateX(0%)",
+            opacity: this.state.isViewed ? '0' : '1'
+        })
+        // const burger = document.querySelector('.burgericon');
+        // const nav = document.querySelector('.nav2-links');
+        // const navLinks = document.querySelector('.nav2-links li');
+        //
+        // if (burger) {
+        //     burger.addEventListener('click', () => {
+        //         nav.classList.toggle('nav-active');
+        //         if (navLinks) {
+        //             navLinks.forEach((link, index) => {
+        //                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7}s`;
+        //             });
+        //         }
+        //     });
+        // }
+
+
+    };
+
+    render() {
+        return (
+            <div className="nav2" style={{position: 'fixed', width: '100%'}}>
+                <Link className="phone-logo" onClick={this.logoClick}><FaHome size={30} style={{cursor: 'pointer'}}/></Link>
+                <div className="samlogo" onClick={this.logoClick} style={{cursor: 'pointer'}}>
+                    <h4>Sam Eskandar</h4>
+                </div>
+                <ul className="nav2-links" style={{transform: this.state.transform, opacity: this.state.opacity, transition: '1s'}}>
+                    <li className="nav-item">
+                        <Link onClick={this.navSlide.bind(this)} to="aboutpage" spy={true} smooth={true} offset={-58} duration={2000}>About</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link onClick={this.navSlide.bind(this)} to="projectpage" spy={true} smooth={true} offset={-70} duration={2000}>Projects</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link onClick={this.navSlide.bind(this)} to="expertisepage" spy={true} smooth={true} offset={-20} duration={2000}>Expertise</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link onClick={this.navSlide.bind(this)} to="contactpage" spy={true} smooth={true} offset={100} duration={2000}>Contact</Link>
+                    </li>
+                </ul>
+                <div className="burgericon" onClick={this.navSlide.bind(this)}>
+                    <div className="line1"/>
+                    <div className="line2"/>
+                    <div className="line3"/>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Navbar2;
