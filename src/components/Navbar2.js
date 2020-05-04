88ed33468260ec0370d396764cac23c1e002d27d
Sam <sam.eskandar00@hotmail.com>
2020-05-04 13:47:25 -0400

    add burgericon animation

--- src/components/Navbar2.js
+++ src/components/Navbar2.js
@@ -11,13 +11,24 @@ class Navbar2 extends React.Component {
     constructor() {
         super();
         this.state = {
+            isMobile: false,
+            counter: 0,
             transform: 'translateX(100%)',
             isViewed: false,
             opacity: '0',
+            opacity3: '1',
             transition: 'opacity 0.75s ease-in',
             burger1: '',
             burger2: '',
             burger3: '',
+            transitionx1: '',
+            transitionx2: '',
+            transitionx3: '',
+            transitiony1: '',
+            transitiony2: '',
+            transitiony3: '',
+            transformBurgerX: '',
+            transformBurgerY: 'translateX(-200%)',
         };
     }
 
@@ -28,7 +39,10 @@ class Navbar2 extends React.Component {
                 transform: 'translateX(100%)',
                 opacity:'0',
                 opacity2: '0',
+                opacity3: '1',
                 transition: 'opacity 0.75s ease-in, transform 0.01s ease-out 0.75s',
+                transformBurgerX: 'translateX(10%)',
+                transformBurgerY: 'translateX(-200%)'
             })
         }
     };
@@ -36,10 +50,20 @@ class Navbar2 extends React.Component {
     navSlide = () => {
         this.setState({
             isViewed: !this.state.isViewed,
+            counter: this.state.counter++,
+            transitionx1: 'opacity 0.5s ease-in, transform 0.5s ease-in',
+            transitionx2: 'opacity 0.5s ease-in 0.2s, transform 0.5s ease-in 0.2s',
+            transitionx3: 'opacity 0.5s ease-in 0.4s, transform 0.5s ease-in 0.4s',
+            transitiony1: 'opacity 0.5s ease-in, transform 0.5s ease-in',
+            transitiony2: 'opacity 0.5s ease-in 0.2s, transform 0.5s ease-in 0.2s',
+            transitiony3: 'opacity 0.5s ease-in 0.4s, transform 0.5s ease-in 0.4s',
             transform: this.state.isViewed ? 'translateX(100%)' : "translateX(0%)",
             opacity: this.state.isViewed ? '0' : '1',
             opacity2: this.state.isViewed ? '0' : '0.8',
+            opacity3: this.state.isViewed ? '1' : '0',
             transition: this.state.isViewed ? 'opacity 0.75s ease-in, transform 0.01s ease-out 0.75s' : 'opacity 0.75s ease-in',
+            transformBurgerX: this.state.isViewed ? 'translateX(10%)' : 'translateX(-200%)',
+            transformBurgerY: this.state.isViewed ? 'translateX(-200%)' : 'translateX(10%)',
         })
     };
 
@@ -67,9 +91,14 @@ class Navbar2 extends React.Component {
                         </li>
                     </ul>
                 <div className="burgericon" onClick={this.navSlide.bind(this)}>
-                    <div className="line1"/>
-                    <div className="line2"/>
-                    <div className="line3"/>
+                    <div style={{transform: this.state.transformBurgerX, transition: this.state.transitionx1, opacity: this.state.opacity3}} className="line1"/>
+                    <div style={{transform: this.state.transformBurgerX, transition: this.state.transitionx2, opacity: this.state.opacity3}} className="line2"/>
+                    <div style={{transform: this.state.transformBurgerX, transition: this.state.transitionx3, opacity: this.state.opacity3}} className="line3"/>
+                </div>
+                <div className="burgericon-rotate" onClick={this.navSlide.bind(this)}>
+                    <div style={{transform: this.state.transformBurgerY, transition: this.state.transitiony1, opacity: this.state.opacity}} className="line1"/>
+                    <div style={{transform: this.state.transformBurgerY, transition: this.state.transitiony2, opacity: this.state.opacity}} className="line2"/>
+                    <div style={{transform: this.state.transformBurgerY, transition: this.state.transitiony3, opacity: this.state.opacity}} className="line3"/>
                 </div>
             </div>
             </div>
