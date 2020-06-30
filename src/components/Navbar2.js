27b0f9f48e4038a9725ed67dde2fdd5891d3da8c
Sam <sam.eskandar00@hotmail.com>
2020-06-29 22:24:24 -0400

    add featured blogs to nav

--- src/components/Navbar2.js
+++ src/components/Navbar2.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link, animateScroll as scroll} from 'react-scroll';
 import {FaHome} from 'react-icons/fa';
-import { Link as RouteLink } from 'react-router-dom';
+import { AutoRotatingCarousel, Slide } from "material-auto-rotating-carousel";
+import { blue } from '@material-ui/core/colors'
+
+import jshooks from '../media/react_hooks.png';
+import reactTs from '../media/react_hooks_ts.png'
 
 class Navbar2 extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             isMobile: false,
             counter: 0,
@@ -25,8 +29,11 @@ class Navbar2 extends React.Component {
             transitiony3: '',
             transformBurgerX: '',
             transformBurgerY: 'translateX(-200%)',
+            open: false,
         };
     }
+
+    isMobile = window.innerWidth < 600;
 
     logoClick = () => {
         scroll.scrollToTop();
@@ -63,43 +70,89 @@ class Navbar2 extends React.Component {
         })
     };
 
+    openCarousel = () => {
+        this.setState({
+            open: true
+        })
+    }
+
     render() {
         return (
-            <div>
-            <div className="background-dark" style={{transform: this.state.transform, opacity: this.state.opacity2, transition: this.state.transition}}/>
-            <div className="nav2" style={{position: 'fixed', width: '100%'}}>
-                <Link className="phone-logo" onClick={this.logoClick}><FaHome size={30} style={{cursor: 'pointer'}}/></Link>
-                <div className="samlogo" onClick={this.logoClick} style={{cursor: 'pointer'}}>
-                    <h4>Sam Eskandar</h4>
+            <div onClick={this.navSlide}>
+                <div className="background-dark" style={{transform: this.state.transform, opacity: this.state.opacity2, transition: this.state.transition}}/>
+                <div className="nav2" style={{position: 'fixed', width: '100%'}}>
+                    <Link className="phone-logo" onClick={this.logoClick}><FaHome size={30} style={{cursor: 'pointer'}}/></Link>
+                    <div className="samlogo" onClick={this.logoClick} style={{cursor: 'pointer'}}>
+                        <h4>Sam Eskandar</h4>
+                    </div>
+                        <ul className="nav2-links" style={{transform: this.state.transform, opacity: this.state.opacity, transition: '1s'}}>
+                            <li className="nav-item">
+                                <Link onClick={this.navSlide.bind(this)} to="aboutpage" spy={true} smooth={true} offset={-58} duration={2000}>About</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link onClick={this.navSlide.bind(this)} to="projectpage" spy={true} smooth={true} offset={-70} duration={2000}>Projects</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link onClick={this.openCarousel}>Tech Blog</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link onClick={this.navSlide.bind(this)} to="contactpage" spy={true} smooth={true} offset={100} duration={2000}>Contact</Link>
+                            </li>
+                        </ul>
+                    <div className="burgericon" onClick={this.navSlide.bind(this)}>
+                        <div style={{transform: this.state.transformBurgerX, transition: this.state.transitionx1, opacity: this.state.opacity3}} className="line1"/>
+                        <div style={{transform: this.state.transformBurgerX, transition: this.state.transitionx2, opacity: this.state.opacity3}} className="line2"/>
+                        <div style={{transform: this.state.transformBurgerX, transition: this.state.transitionx3, opacity: this.state.opacity3}} className="line3"/>
+                    </div>
+                    <div className="burgericon-rotate" onClick={this.navSlide.bind(this)}>
+                        <div style={{transform: this.state.transformBurgerY, transition: this.state.transitiony1, opacity: this.state.opacity}} className="line1"/>
+                        <div style={{transform: this.state.transformBurgerY, transition: this.state.transitiony2, opacity: this.state.opacity}} className="line2"/>
+                        <div style={{transform: this.state.transformBurgerY, transition: this.state.transitiony3, opacity: this.state.opacity}} className="line3"/>
+                    </div>
                 </div>
-                    <ul className="nav2-links" style={{transform: this.state.transform, opacity: this.state.opacity, transition: '1s'}}>
-                        <li className="nav-item">
-                            <Link onClick={this.navSlide.bind(this)} to="aboutpage" spy={true} smooth={true} offset={-58} duration={2000}>About</Link>
-                        </li>
-                        {/*<li className="nav-item">*/}
-                        {/*    <RouteLink style={{textDecoration: 'none', color: 'black'}} to="/blog">Blog</RouteLink>*/}
-                        {/*</li>*/}
-                        <li className="nav-item">
-                            <Link onClick={this.navSlide.bind(this)} to="projectpage" spy={true} smooth={true} offset={-70} duration={2000}>Projects</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link onClick={this.navSlide.bind(this)} to="expertisepage" spy={true} smooth={true} offset={-20} duration={2000}>Expertise</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link onClick={this.navSlide.bind(this)} to="contactpage" spy={true} smooth={true} offset={100} duration={2000}>Contact</Link>
-                        </li>
-                    </ul>
-                <div className="burgericon" onClick={this.navSlide.bind(this)}>
-                    <div style={{transform: this.state.transformBurgerX, transition: this.state.transitionx1, opacity: this.state.opacity3}} className="line1"/>
-                    <div style={{transform: this.state.transformBurgerX, transition: this.state.transitionx2, opacity: this.state.opacity3}} className="line2"/>
-                    <div style={{transform: this.state.transformBurgerX, transition: this.state.transitionx3, opacity: this.state.opacity3}} className="line3"/>
+
+                <div>
+                    <AutoRotatingCarousel
+                        label="Take Me There!"
+                        open={this.state.open}
+                        onClose={() => this.setState({open: false})}
+                        onStart={() => window.open('https://samtechblog.com')}
+                        autoplay={false}
+                        mobile={this.isMobile}
+                        style={{ position: "absolute" }}
+                    >
+                        <Slide
+                            onClick={this.isMobile ? () => this.setState({open: false}) : null}
+                            media={
+                                <img src="https://www.samtechblog.com/static/media/django_django_unchained.93d7fd73.png" />
+                            }
+                            mediaBackgroundStyle={{ backgroundColor: '#4dabf5' }}
+                            style={{ backgroundColor: '#4dabf5' }}
+                            title="Introduction to Django"
+                            subtitle="Building a Full Stack book rating applcation with Django!"
+                        />
+                        <Slide
+                            onClick={this.isMobile ? () => this.setState({open: false}) : null}
+                            media={
+                                <img src={jshooks} />
+                            }
+                            mediaBackgroundStyle={{ backgroundColor: blue[400] }}
+                            style={{ backgroundColor: '#797A7B' }}
+                            title="React Hooks with JavaScript"
+                            subtitle="Coding and fishing. Name a better combo. I'll wait."
+                        />
+                        <Slide
+                            media={
+                                <img src={reactTs} />
+                            }
+                            onClick={this.isMobile ? () => this.setState({open: false}) : null}
+                            mediaBackgroundStyle={{ backgroundColor: blue[400] }}
+                            style={{ backgroundColor: blue[600] }}
+                            title="TypeScript and React"
+                            subtitle="Going through a breakup isn't easy. Especially not one with JavaScript"
+                        />
+                    </AutoRotatingCarousel>
                 </div>
-                <div className="burgericon-rotate" onClick={this.navSlide.bind(this)}>
-                    <div style={{transform: this.state.transformBurgerY, transition: this.state.transitiony1, opacity: this.state.opacity}} className="line1"/>
-                    <div style={{transform: this.state.transformBurgerY, transition: this.state.transitiony2, opacity: this.state.opacity}} className="line2"/>
-                    <div style={{transform: this.state.transformBurgerY, transition: this.state.transitiony3, opacity: this.state.opacity}} className="line3"/>
-                </div>
-            </div>
             </div>
         )
     }
