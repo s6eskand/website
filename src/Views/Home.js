5c319c5f7bb89b0e7403f532cf18d0dbc7dc50e7
Sam <sam.eskandar00@hotmail.com>
2020-06-29 22:22:38 -0400

    move navbar

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -2,17 +2,18 @@ import React from 'react';
 import Header from "../components/Header";
 import About from "../components/About";
 import Projects from "../components/Projects";
-import Expertise from "../components/Expertise";
 import Contact from "../components/Contact";
+import Navbar2 from "../components/Navbar2";
 
 class Home extends React.Component {
+
     render() {
         return (
             <div>
+                <Navbar2 />
                 <Header/>
                 <About/>
                 <Projects/>
-                <Expertise/>
                 <Contact/>
             </div>
         );
